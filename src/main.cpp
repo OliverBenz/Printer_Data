7e5bd1bff6d2075469129add367451c3100fb536
OliverBenz <oliver.benz@outlook.com>
2020-10-06 18:46:19 +0200

    Removed using namespace std

--- src/main.cpp
+++ src/main.cpp
@@ -10,53 +10,51 @@
 #include "lib/Time.hpp"
 #include "lib/Print.hpp"
 
-using namespace std;
 using namespace Printer::Data;
 
 // Analytic functions
-void mostPrints(vector<Print>& prints);
+void mostPrints(std::vector<Print>& prints);
 
 int main() {
-	ifstream file("res/prints.csv");
+	std::ifstream file("res/prints.csv");
 
-	vector<Print> prints;
+	std::vector<Print> prints;
 
 	if (! file.is_open()){
-		cout << "Could not open file" << endl;
+		std::cout << "Could not open file.\n";
 		return -1;
 	}
 
-	string line;
-	while (getline(file, line))
+	std::string line;
+	while (std::getline(file, line))
 		prints.push_back(Print(line));
 	file.close();
 
-	cout << "Person with the most prints: ";
+	std::cout << "Person with the most prints: ";
 	mostPrints(prints);
 
-	cout << prints[17] << endl;
+	std::cout << prints[17] << std::endl;
 
 	return 0;
 }
 
-void mostPrints(vector<Print>& prints) {
-	map<string, int> userPrints;
+void mostPrints(std::vector<Print>& prints) {
+	std::map<std::string, int> userPrints;
 	
 	// Insert users and printCount to map
 	for (auto& i : prints) {
 		if (userPrints.find(i.getUser()) != userPrints.end())
 			userPrints.find(i.getUser())->second += 1;
-		else {
+		else
 			userPrints.insert(make_pair(i.getUser(), 1));
-		}
 	}
 
 	// Get pair with most prints from map
-	pair<string, int> most = *userPrints.begin();
+	std::pair<std::string, int> most = *userPrints.begin();
 	for (auto& i : userPrints)
 		if (i.second > most.second)
 			most = i;
 
-	cout << most.first << " - " << most.second << endl;
+	std::cout << most.first << " - " << most.second << "\n";
 }
 
